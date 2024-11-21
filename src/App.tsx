cd9e7bb5cb0e7d418ecc48868720ce0f2cac61c4
Fleege <76089935+pfleege@users.noreply.github.com>
2024-11-21 17:42:30 +0200

    ADD Navbar component and navbar images

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Web Store</h1>
       <p>Welcome to the Web Store!</p>
     </>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+  return (
+    <>
+      <img src="src/assets/images/BeanyCupLogo.png" alt="Webshop logo" />
+      <div className="navigation">
+        <div className="navlinks">
+          <a href="#">Home</a>
+          <a href="#">Shop</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+      <div className="shoppingCart">
+        <img src="src/assets/images/shoppingCart.png" alt="Shopping cart" />
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
+
+// Logo font: Monotype Corsiva 48px & 28px
