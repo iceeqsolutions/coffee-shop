c94905340815fd690f401b2dc6b81dae28536c01
Fleege <76089935+pfleege@users.noreply.github.com>
2024-12-01 12:00:07 +0200

    ADD styling to ShoppingCart page
    - Restructure and reuse the ShoppingCartButton component

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,25 +1,30 @@
-import { useDispatch, useSelector } from "react-redux";
-import { addToCart, removeFromCart } from "../redux/features/cartSlice";
+import { useSelector } from "react-redux";
 import { RootState } from "../redux/store";
+import ShoppingCartButton from "../components/ShoppingCartButton";
 
 const ShoppingCart: React.FC = () => {
-  const dispatch = useDispatch();
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
 
   return (
     <>
-      <h1>Shopping Cart</h1>
-      {cartItems.map((item) => (
-        <div key={item.id}>
-          <h2>{item.name}</h2>
-          <img src={item.image_url} alt={item.name} />
-          <div>
-            <button onClick={() => dispatch(removeFromCart(item.id))}>-</button>
-            <span>{item.quantity}</span>
-            <button onClick={() => dispatch(addToCart(item))}>+</button>
-          </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-3xl my-10">Shopping Cart</h1>
+        <div className="w-3/4 flex flex-col m-10 justify-center bg-neutral-50 rounded-3xl shadow-2xl">
+          {cartItems.map((item) => (
+            <div className="m-5 flex flex-col items-start" key={item.id}>
+              <h2 className="text-xl font-bold text-yellow-950">{item.name}</h2>
+              <div className="w-full flex justify-between items-center">
+                <img
+                  className="h-52 aspect-square object-cover object-center"
+                  src={item.image_url}
+                  alt={item.name}
+                />
+                <ShoppingCartButton item={item} />
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 };
