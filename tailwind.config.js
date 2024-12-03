7fc9f15f80a7e1ffd99235a048f36603aff861f9
Fleege <76089935+pfleege@users.noreply.github.com>
2024-12-03 20:02:31 +0200

    ADD page banners to custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        NotFound: "url('src/assets/images/notFoundBanner.jpg')",
+        CoffeeShop: "url('src/assets/images/shopBanner.jpg')",
+        ShoppingCart: "url('src/assets/images/coffeeShopBanner.jpg')",
+        Contact: "url('src/assets/images/contactUs.jpg')",
+      },
       keyframes: {
         wiggle: {
           "0%, 100%": { transform: "rotate(-0.5deg)" },
