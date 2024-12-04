14f553ab0e6f73310520d6c961326f2732bd9a1b
Fleege <76089935+pfleege@users.noreply.github.com>
2024-12-04 10:24:20 +0200

    ADD responsive design to contact and shop pages

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,3 @@
-import contactUs from "../assets/images/contactUs.jpg";
 import contactText from "../assets/texts/contactTexts";
 import FeedbackForm from "../components/FeedbackForm";
 
@@ -6,47 +5,38 @@ const Contact: React.FC = () => {
   return (
     <>
       <div className="mt-48 flex flex-col justify-center items-center">
-        <div className="banner relative">
-          <div className="absolute top-6 left-20">
+        <div className="w-full h-80 bg-cover bg-right bg-no-repeat bg-Contact relative">
+          <div className="absolute top-10 left-20 text-white italic font-charm">
             <h2 className="text-8xl">We're here</h2>
-            <h3 className="pt-12 text-5xl">
-              ...and we'd love to hear from you
-            </h3>
+            <h3 className="pt-4 text-5xl">...and we'd love to hear from you</h3>
           </div>
-          <img
-            src={contactUs}
-            alt="Contact banner"
-            className="w-full bg-contain"
-          />
         </div>
-        <div className="mt-10 flex flex-col gap-2 p-2">
-          <section className="px-16 py-6 flex flex-col gap-12 w-full laptop:px-4">
-            <div className="flex gap-8 tablet:flex-col">
-              {contactText.map((item) => (
-                <div className="flex flex-col gap-2 flex-1" key={item.header}>
-                  <h3 className="text-lg uppercase font-semibold flex gap-1 items-center">
-                    {item.header}
-                  </h3>
-                  <p>{item.text}</p>
-                  <img
-                    className="object-contain width-full"
-                    src={item.image}
-                    alt="Coffee shop"
-                  />
-                  <h2 className="p-2 text-2xl">
-                    {item.header === "Visit Us"
-                      ? "Our address: "
-                      : item.header === "Call Us"
-                      ? "Our Phone: "
-                      : "Our Email: "}
-                    <span className="text-yellow-900 font-bold text-xl">
-                      {item.contact}
-                    </span>
-                  </h2>
-                </div>
-              ))}
-            </div>
-          </section>
+        <div className="w-full mt-10 flex gap-2 p-2 justify-center">
+          <div className="flex gap-8 flex-wrap justify-center">
+            {contactText.map((item) => (
+              <div className="flex flex-col gap-2 w-[400px]" key={item.header}>
+                <h3 className="text-lg uppercase font-semibold flex gap-1 items-center">
+                  {item.header}
+                </h3>
+                <p className="min-h-20">{item.text}</p>
+                <img
+                  className="object-contain width-full"
+                  src={item.image}
+                  alt="Coffee shop"
+                />
+                <h2 className="p-2 text-2xl">
+                  {item.header === "Visit Us"
+                    ? "Our address: "
+                    : item.header === "Call Us"
+                    ? "Our Phone: "
+                    : "Our Email: "}
+                  <span className="text-yellow-900 font-bold text-xl">
+                    {item.contact}
+                  </span>
+                </h2>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <FeedbackForm />
--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,6 +1,5 @@
 import ApiConnection from "../modules/ApiConnection";
 import { ShopInterface } from "../interfaces/ShopInterface";
-import shopBanner from "../assets/images/shopBanner.jpg";
 
 const Shop: React.FC<ShopInterface> = ({
   numberOfCartItems,
@@ -12,13 +11,10 @@ const Shop: React.FC<ShopInterface> = ({
   return (
     <>
       <div className="mt-48 flex flex-col justify-center items-center">
-        <div className="banner relative">
-          <h2 className="absolute top-16 left-20">Coffee Shop</h2>
-          <img
-            src={shopBanner}
-            alt="Shop Banner"
-            className="w-full bg-contain"
-          />
+        <div className="w-full h-80 bg-cover bg-right bg-no-repeat bg-CoffeeShop relative">
+          <h2 className="font-charm text-white text-8xl italic absolute top-16 left-20">
+            Coffee Shop
+          </h2>
         </div>
         <div className="mt-32 flex flex-col gap-8 justify-center w-full">
           <ApiConnection
