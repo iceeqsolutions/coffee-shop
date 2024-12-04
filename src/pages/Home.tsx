dd8a939b833a2580d5a2a04652e1ca408b0a447e
Fleege <76089935+pfleege@users.noreply.github.com>
2024-12-04 08:26:43 +0200

    ADD custom CSS to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,10 +13,12 @@ const Home: React.FC<HomeInterface> = ({
   return (
     <>
       <div className="mt-48 flex flex-col justify-center h-[573px] relative z-2 text-body-clr">
-        <div className="h-80 bannerHeading flex flex-col justify-between absolute top-24 left-24">
-          <h1>Fresh coffee from around the world...</h1>
+        <div className="h-80 bannerHeading flex flex-col justify-between absolute top-20 left-24">
+          <h1 className="font-charm text-white text-8xl italic">
+            Fresh coffee from around the world...
+          </h1>
           <Link
-            className="ml-10 px-5 pt-2 pb-3 w-fit rounded-xl text-2xl text-white border-2 border-white bg-opacity-75  hover:bg-white hover:text-yellow-950 hover:shadow-yellow-50 hover:shadow-[0_0_20px_2px]"
+            className="ml-10 mt-5 px-5 pt-2 pb-3 w-fit rounded-xl text-2xl text-white border-2 border-white bg-opacity-75  hover:bg-white hover:text-yellow-950 hover:shadow-yellow-50 hover:shadow-[0_0_20px_2px]"
             to="./shop"
           >
             Shop Now
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.25rem" }],
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      lg: ["1.125rem", { lineHeight: "1.75rem" }],
+      xl: ["1.25rem", { lineHeight: "1.75rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+      "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+      "5xl": ["3rem", { lineHeight: "1" }],
+      "6xl": ["4rem", { lineHeight: "1" }],
+      "7xl": ["5rem", { lineHeight: "1" }],
+      "8xl": [
+        "6rem",
+        {
+          letterSpacing: "2px",
+          lineHeight: "8rem",
+        },
+      ],
+    },
+    fontFamily: {
+      charm: ["Charm", "cursive"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {
       backgroundImage: {
         NotFound: "url('src/assets/images/notFoundBanner.jpg')",
